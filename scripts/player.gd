eda967d4eb502295f9dbb64032bd7ecb04d97530
Helheimr1711 <david.ostry09@gmail.com>
2025-06-18 19:20:37 +0200

    Added headbobbing

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody3D
 
-@onready var camera = $Camera3D
+@onready var camera = $eyes/Camera3D
+@onready var eyes = $eyes
 
 ## Movement
 # Speed variables
@@ -21,9 +22,17 @@ var jump_stamina_consumption := 11.
 var sprinting_stamina_consumption := 0.5
 var stamina_wait_time := 2.
 var stamina_cooldown := 0.
-
+# Mouse sensitivies
 var mouse_sens := 0.25
 var jumping_mouse_sens := 0.05
+## Headbobbing
+# Values
+const headbobbing_values := {"Intensity" : {"walking" : 0.1, "sprinting" : 0.2, "none" : 0.0},
+	"Speed" : {"walking" : 14.0, "sprinting" : 22.0, "none" : 0.0},}
+# Variables
+var headbobbing_vector := Vector2.ZERO
+var headbobbing_index := 0.0
+var headbobbing_current_intensity := 0.0
 
 
 func _ready() -> void:
@@ -51,15 +60,39 @@ func regenerate_stamina() -> void:
 	if stamina < stamina_max:
 		stamina += 1
 
+## Headbobbing functions
+
+# Headbobbing intensity and speed values setting
+func set_headbobbing_values(delta: float, current_movement) -> void:
+	headbobbing_current_intensity = headbobbing_values["Intensity"][current_movement]
+	headbobbing_index += headbobbing_values["Speed"][current_movement] * delta
+
+# Headbobbing vector setting
+func set_headbobbing_vector() -> void:
+	headbobbing_vector.y = sin(headbobbing_index)
+	headbobbing_vector.x = sin(headbobbing_index / 2) + 0.5
+
+# Headbobbing eyes position setting
+func set_headbobbing_eyes_position(delta: float) -> void:
+	eyes.position.y = lerp(eyes.position.y, headbobbing_vector.y * (headbobbing_current_intensity / 2.0), delta * lerp_speed)
+	eyes.position.x = lerp(eyes.position.x, headbobbing_vector.x * headbobbing_current_intensity, delta * lerp_speed)
+
+# Headbobbing master function combining all functions of headbobbing.
+func headbobbing(delta: float, current_movement) -> void:
+	set_headbobbing_values(delta, current_movement)
+	set_headbobbing_vector()
+	set_headbobbing_eyes_position(delta)
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
+	# Stamina cooldown lowering.
 	if stamina_cooldown > 0.:
 		stamina_cooldown -= delta
 
+	# Stamina regenerating
 	if stamina_cooldown <= 0.:
 		regenerate_stamina()
 
@@ -82,6 +115,7 @@ func _physics_process(delta: float) -> void:
 			# Checking if player has enough stamina to sprint.
 			if stamina - sprinting_stamina_consumption >= 0:
 				SPEED = SPRINT_SPEED
+				headbobbing(delta, "sprinting")
 				use_stamina(sprinting_stamina_consumption)
 			# Set when there is not enough stamina.
 			else:
@@ -97,12 +131,14 @@ func _physics_process(delta: float) -> void:
 	if is_on_floor():
 		var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 		direction = lerp(direction, (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized(), delta * lerp_speed)
-		if direction:
+		if direction.length() > 0.03:
 			velocity.x = direction.x * SPEED
 			velocity.z = direction.z * SPEED
+			headbobbing(delta, "walking")
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
 			velocity.z = move_toward(velocity.z, 0, SPEED)
+			headbobbing(delta, "none")
 
 	move_and_slide()
-	print(stamina)
+	print(direction)
