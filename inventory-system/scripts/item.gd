51fb8958749030f01a58479750865a278c778a75
Helheimr1711 <david.ostry09@gmail.com>
2025-06-27 15:12:49 +0200

    item id creator and mesh loader based on object template

--- inventory-system/scripts/item.gd
+++ inventory-system/scripts/item.gd
@@ -3,4 +3,32 @@ extends Node3D
 @onready var item_mesh = $StaticBody3D/MeshInstance3D
 @onready var item_collision = $StaticBody3D/CollisionShape3D
 
-@export var template_item : 
+@export var item_template: String
+var item_id: String
+
+func _ready() -> void:
+	if item_template in ItemsTemplates.items_templates:
+		#adding id                                                                    #adding item type
+		item_id = str(ItemsTemplates.items_templates[item_template].id) + "_" + str(ItemsTemplates.items_templates[item_template].item_type)
+		match ItemsTemplates.items_templates[item_template].item_type:
+			0:
+				#weapon
+				item_id = item_id + "_" + str(ItemsTemplates.items_templates[item_template].mag_size)
+			1:
+				#consumable
+				item_id = item_id + "_" + str(ItemsTemplates.items_templates[item_template].health)
+			2:
+				#valuable
+				pass
+			3:
+				#amunition
+				item_id = item_id + "_" + str(ItemsTemplates.items_templates[item_template].size)
+		
+		print(item_id)
+		
+		item_mesh.mesh = load(ItemsTemplates.FILE_PATH + item_template + ".obj")
+	else:
+		print("Item: " + item_template + " dont exist.")
+
+func _process(delta: float) -> void:
+	print(item_id)
--- inventory-system/scripts/items_templates.gd
+++ inventory-system/scripts/items_templates.gd
@@ -1,11 +1,31 @@
 extends Node
 
+## Variables
+const FILE_PATH = "res://Space_co-op_game/inventory-system/assets/items/meshes/"
+enum item_type {Weapon, Consumable, Valuable, Ammunition}
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+## Items Templates
+const items_templates = {
+	"pistol" : {
+		"id" : 0,
+		"name" : "Pistol",
+		"desc" : "Good pistol for shooting.",
+		"item_type" : item_type.Weapon,
+		"mag_size" : 8,
+		"automatic" : false,
+	},
+	"medkit" : {
+		"id" : 1,
+		"name" : "Med-Kit",
+		"desc" : "Cool medkit",
+		"item_type" : item_type.Consumable,
+		"health" : 25,
+	},
+	"ammo" : {
+		"id" : 2,
+		"name" : "Ammunition",
+		"desc" : "Universal ammunition for all weapons.",
+		"item_type" : item_type.Ammunition,
+		"size" : 30,
+	},
+}
